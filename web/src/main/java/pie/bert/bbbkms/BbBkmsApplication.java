19e21e91095b7983edca7ed8ba5a4080e776ca6a
Bert Pie <BertPieGIT@gmail.com>
2019-01-23 19:17:59 +0100

    init commit multi-module gradle project

---
+++ web/src/main/java/pie/bert/bbbkms/BbBkmsApplication.java
@@ -0,0 +1,14 @@
+package pie.bert.bbbkms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbBkmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbBkmsApplication.class, args);
+	}
+
+}
+
